e1037718d3f7bcb219c818322ca9f9d34e27b1ce
William Desportes <williamdes@wdes.fr>
2026-01-21 23:58:49 +0100

    tests: remove PHP 7 special cases

--- tests/ErrorFormatterTestCase.php
+++ tests/ErrorFormatterTestCase.php
@@ -61,7 +61,7 @@ abstract class ErrorFormatterTestCase extends \CodeLts\CliTools\Tests\AbstractTe
 
     private function getOutputStream(): StreamOutput
     {
-        if (PHP_VERSION_ID >= 80000 && DIRECTORY_SEPARATOR === '\\') {
+        if (DIRECTORY_SEPARATOR === '\\') {
             $this->markTestSkipped('Skipped because of https://github.com/symfony/symfony/issues/37508');
         }
         if ($this->outputStream === null) {
